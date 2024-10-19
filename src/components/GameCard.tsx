0ed8f477569d8da9206c8600eef98848850b9816
Marwan Abdalla <marwan3m2000@gmail.com>
2024-10-19 13:53:57 +0200

    added critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,14 +3,17 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlattformIconList";
 
 function GameCard({ game }: { game: Game }) {
-  const platforms = game.parent_platforms.map(object => object.platform)
+  const platforms = game.parent_platforms.map((object) => object.platform);
   return (
     <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image}></Image>
-        <div className="flex flex-col gap-2 p-6">
-            <h2 className="text-2xl font-semibold">{game.name}</h2>
-            <PlatformIconList platforms={platforms}></PlatformIconList>
+      <Image src={game.background_image}></Image>
+      <div className="flex flex-col gap-2 p-6">
+        <h2 className="text-2xl font-semibold">{game.name}</h2>
+        <div className="flex justify-between">
+          <PlatformIconList platforms={platforms}></PlatformIconList>
+          <div className="py-0.5 px-2 font-semibold text-green-200 rounded-md bg-opacity-20 bg-green-50">{game.metacritic}</div>
         </div>
+      </div>
     </Card>
   );
 }
