d6beb70b7a65cfa8754e80abdb1aabca8f6fb5f3
Marwan Abdalla <marwan3m2000@gmail.com>
2024-10-20 02:46:56 +0200

    some cleanup

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,22 +4,20 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
-  gameQuery: GameQuery
+  gameQuery: GameQuery;
 }
 
-function GameGrid({gameQuery }: Props) {
+function GameGrid({ gameQuery }: Props) {
   const { data, error, loading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  if (error) return <p>{error}</p>;
   return (
-    <>
-      {error && <p>{error}</p>}
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6">
-        {loading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {!loading &&
-          data.map((game) => <GameCard key={game.id} game={game}></GameCard>)}
-      </div>
-    </>
+    <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6">
+      {loading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      {!loading &&
+        data.map((game) => <GameCard key={game.id} game={game}></GameCard>)}
+    </div>
   );
 }
 
